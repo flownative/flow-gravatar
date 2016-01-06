0d25338884bf33fa8f4e7d94541ea5b98821630f
Robert Lemke <robert@flownative.com>
2016-01-06 18:07:22 +0100

    Initial commit

---
+++ Classes/ViewHelpers/GravatarViewHelper.php
@@ -0,0 +1,75 @@
+<?php
+namespace Flownative\Gravatar\ViewHelpers;
+
+/*
+ * This file is part of the Flownative.Gravatar package.
+ *
+ * (c) Robert Lemke, Flownative GmbH - www.flownative.com
+ */
+
+use TYPO3\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
+/**
+ * A view helper to display a Gravatar
+ *
+ * You may specify the size (width = height) in pixels and a fallback / default image URL. Instead of a URL, the
+ * "default" parameter can also be one of "404", "mm", "identicon", "monsterid", "wavatar", retro" or "blank".
+ *
+ * See https://en.gravatar.com/site/implement/images/ for a full explanation of these options.
+ *
+ * = Examples =
+ *
+ * <code title="Simple">
+ * <flownative:gravatar email="{emailAddress}" default="http://domain.com/gravatar_default.png" class="gravatar" />
+ * </code>
+ *
+ * Output:
+ * <img class="gravatar" src="http://www.gravatar.com/avatar/<hash>?d=http%3A%2F%2Fdomain.com%2Fgravatar_default.png" />
+ *
+ */
+class GravatarViewHelper extends AbstractTagBasedViewHelper {
+
+    /**
+     * @var string
+     */
+    protected $tagName = 'img';
+
+    /**
+     * Initialize arguments
+     *
+     * @return void
+     */
+    public function initializeArguments() {
+        parent::initializeArguments();
+        $this->registerTagAttribute('alt', 'string', 'Specifies an alternate text for an image', false);
+        $this->registerTagAttribute('width', 'string', 'Specifies the image width', false);
+        $this->registerTagAttribute('height', 'string', 'Specifies the image height', false);
+        $this->registerArgument('email', 'string', 'Gravatar Email', true);
+        $this->registerArgument('default', 'string', 'Default URL if no Gravatar was found');
+        $this->registerArgument('size', 'Integer', 'Size of the Gravatar');
+
+        $this->registerUniversalTagAttributes();
+    }
+
+    /**
+     * Render the link.
+     *
+     * @return string The rendered link
+     */
+    public function render() {
+        $sanitizedEmail = strtolower(trim((string)$this->arguments['email']));
+        $gravatarUri = 'https://www.gravatar.com/avatar/' . md5($sanitizedEmail);
+        $uriParts = array();
+        if ($this->arguments['default']) {
+            $uriParts[] = 'd=' . urlencode($this->arguments['default']);
+        }
+        if ($this->arguments['size']) {
+            $uriParts[] = 's=' . $this->arguments['size'];
+        }
+        if (count($uriParts)) {
+            $gravatarUri .= '?' . implode('&', $uriParts);
+        }
+        $this->tag->addAttribute('src', $gravatarUri);
+        return $this->tag->render();
+    }
+}
---
+++ README.rst
@@ -0,0 +1,6 @@
+Gravatar View Helper for Flow
+-----------------------------
+
+This package provides a Fluid View Helper which can be used in Flow Framework based applications for displaying
+Gravatar images.
+
